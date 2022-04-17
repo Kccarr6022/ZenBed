430a5404eb99aa62522a628196fcd8de3007c524
Kaden Carr <Kccarr6022@eagle.fgcu.edu>
2022-04-16 22:55:32 -0400

    Included new packages

--- Classes/zenbedclass.py
+++ Classes/zenbedclass.py
@@ -1,1 +1,3 @@
-
+class ZenBed:
+    def Dialog(self):
+        print("I am the ZenBed")
--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
+from Classes.zenbedclass import ZenBed
+from Classes.motorclass import Motor
 import sys
-import ServoKit
 import time
+from adafruit_servokit import ServoKit
 
 
 def main(args):
     return 0
 
 if __name__ == '__main__':
+    main()
