74ba6bbfb64f946448a891da8e3d19c04c0afe91
Kaden Carr <71856219+Kccarr6022@users.noreply.github.com>
2022-04-30 15:36:46 -0400

    Library commands for individual motor.

--- Main.py
+++ Main.py
@@ -1,12 +1,30 @@
-from Classes.motorclass import Motor
-from Classes.zenbedclass import ZenBed
+#from zenbedclass import ZenBed
+#from motorclass import Motor
 import sys
 import time
-from adafruit_servokit import ServoKit
+from board import SCL, SDA
+import busio
+#from adafruit_servokit import ServoKit
+from adafruit_pca9685 import PCA9685
 
+def init():
+    #Creates Motorboard object
+    i2c_bus = busio.I2C(SCL, SDA)
+    MotorBoard = PCA9685(i2c_bus)
+    MotorBoard.frequency = 60
+    pass
 
-def main(args):
-    return 0
+def main():
+    
+    #Initialization
+    i2c_bus = busio.I2C(SCL, SDA)
+    MotorBoard = PCA9685(i2c_bus)
+    MotorBoard.frequency = 60
+    
+    #Turns on motor 0
+    MotorBoard.channels[0].duty_cycle = 0 # Full power for motor
+    pass
 
 if __name__ == '__main__':
+    init()
     main()
