d280cbc3c38b997a91815ed31f7487b74b6c77e3
Kaden Carr <71856219+Kccarr6022@users.noreply.github.com>
2022-06-26 18:54:03 -0400

    Improved formatting

--- Main.py
+++ Main.py
@@ -1,10 +1,6 @@
 from motorclass import Motor
 from zenbedclass import ZenBed
 
-
-# creates i2c object
-i2c_bus = busio.I2C(SCL, SDA)
-
 # Letters
 A = 1
 B = 2
@@ -23,10 +19,6 @@ L = 12
 MOTORGRIDXSIZE = 12
 MOTORGRIDYSIZE = 18
 
-
-
-
-         
 """
     Motor on -> Zenbed.mtr[letter][number].percent(percent power)
     Pattern -> variable_name = "Capital letter first (X coordinate) -> number (Y coordinate) -> comma (Seperates the elements grouped)" 
@@ -46,30 +38,26 @@ MOTORGRIDYSIZE = 18
             Zenbed.mtr[x][y].percent(20) #Set all motors to power level
     """
 
+rectangle = "D4 E4, D5 E5, D6 E6, D7 E7, D8 E8, D9 E9, D10 E10, D11 E11, D12 E12, D13 E13, D14 E14, D15 E15, " \
+            "D16 D17 E16 E17, F16 F17, G16 G17, H16 H17 I16 I17, H15 I15, H14 I14, H13 I13, H12 I12, H11 I11, " \
+            "H10 I10, H9 I9, H8 I8, H7 I7, H6 I6, H5 I5, H4 I4, H3 I3 H2 I2, G2 G3, F2 F3, D2 E2 Dsd3 E3 "
 
+pattern_1 = "A1, B2, C3, D4, E5, F6, G7 H7, G8 H8, G9 H9, G10 H10, G11 G12 H11 H12, F11 F12, E11 E12, D11 D12, " \
+            "C11 C12, B11 B12, A11 A12, A10, A9, A8, A7, A6, A5, A4, A3, A2, A1 "
 
-def pattern(bed, pattern):
-    bed.sequence_to_pattern(bed.string_to_seq(pattern))
-
-rectangle = "D4 E4, D5 E5, D6 E6, D7 E7, D8 E8, D9 E9, D10 E10, D11 E11, D12 E12, D13 E13, D14 E14, D15 E15, D16 D17 E16 E17, F16 F17, G16 G17, H16 H17 I16 I17, H15 I15, H14 I14, H13 I13, H12 I12, H11 I11, H10 I10, H9 I9, H8 I8, H7 I7, H6 I6, H5 I5, H4 I4, H3 I3 H2 I2, G2 G3, F2 F3, D2 E2 Dsd3 E3"
-
-randodiagnal = "A1, B2, C3, D4, E5, F6, G7 H7, G8 H8, G9 H9, G10 H10, G11 G12 H11 H12, F11 F12, E11 E12, D11 D12, C11 C12, B11 B12, A11 A12, A10, A9, A8, A7, A6, A5, A4, A3, A2, A1"
 
 def main():
-    Zenbed = ZenBed()
-    #rectangle_array = Zenbed.string_to_seq(Zenbed.string_rectangle)
-    Zenbed.pattern_rate_of_change = 15
-    Zenbed.pattern_max_power = 40
-    Zenbed.pattern_start_power = 10
-    #Zenbed.sequence_to_pattern(Zenbed.rectangle)
-    #pattern(Zenbed, Zenbed.string_rectangle)
-    #Zenbed.all_motors_increase()
-    #Zenbed.pattern(rectangle)
-    #Zenbed.mtr[B][18].percent(30) #Set the power level of a single motor
-    #Zenbed.on(20) #Also sets all motors to power level
-    Zenbed.status()
+    zenbed = ZenBed()
+    # rectangle_array = Zenbed.string_to_seq(Zenbed.string_rectangle)
+    zenbed.pattern_rate_of_change = 15
+    zenbed.pattern_max_power = 40
+    zenbed.pattern_start_power = 10
+
+    zenbed.pattern(rectangle)
+    zenbed.status()
     time.sleep(10)
-    Zenbed.off()
-    
-    
+    zenbed.off()
+    return 0
+
+
 main()
